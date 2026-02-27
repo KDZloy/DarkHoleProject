7ac7a7754d4c5370abd4ebfdcc2ff430e0d58ff4
KDZloy <parolzloy@gmail.com>
2026-02-27 03:42:55 +0300

    .

--- DarkHole/Assets/Scripts/For player/PlayerMiner.cs
+++ DarkHole/Assets/Scripts/For player/PlayerMiner.cs
@@ -1,31 +1,50 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerMiner : MonoBehaviour
 {
-    [SerializeField] private Camera playerCamera;   // Перетащите сюда Main Camera
-    [SerializeField] private Animator animator;      // Аниматор кирки
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private Animator animator;
     [SerializeField] private float miningRange = 5f;
     [SerializeField] private int damagePerHit = 10;
 
-    private const string ANIM_ATTACK = "Mine"; // Имя триггера в Animator
+    private const string ANIM_ATTACK = "Mine";
+    
+    // Флаг блокировки повторного нажатия
+    private bool isMining = false;
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        // Проверяем нажатие и доступность действия
+        if (Input.GetMouseButtonDown(0) && !isMining)
         {
-            // Запуск анимации
+            isMining = true;
+
+            // Запуск анимации (срабатывает сразу)
             animator.SetTrigger(ANIM_ATTACK);
 
-            // Raycast из центра экрана
-            Ray ray = playerCamera.ViewportPointToRay(Vector2.one * 0.5f); // Центр экрана
-            if (Physics.Raycast(ray, out RaycastHit hit, miningRange))
+            // Запуск корутины с задержкой удара (1.5 сек)
+            StartCoroutine(MineWithDelay());
+        }
+    }
+
+    private IEnumerator MineWithDelay()
+    {
+        // Ждем 1.5 секунды (время анимации)
+        yield return new WaitForSeconds(1.5f);
+
+        // Нанесение урона ПОСЛЕ задержки
+        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        
+        if (Physics.Raycast(ray, out RaycastHit hit, miningRange))
+        {
+            if (hit.collider.TryGetComponent(out StonePile stonePile))
             {
-                // Проверяем, попали ли в каменную кучу
-                if (hit.collider.TryGetComponent(out StonePile stonePile))
-                {
-                    stonePile.TakeDamage(damagePerHit);
-                }
+                stonePile.TakeDamage(damagePerHit);
             }
         }
+
+        // Разблокировка возможности майнить снова
+        isMining = false;
     }
 }
